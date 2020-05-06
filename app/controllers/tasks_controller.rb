969c4902e7b05ee5b6eae01324321ff1063ca2b0
ryohei udagawa <r.udagawa1995@gmail.com>
2020-05-06 09:51:43 +0900

    ステップ１、２実装完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,18 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: %i[show edit update destroy]
 
-  # GET /tasks
   def index
-    @tasks = Task.all
+    @tasks = Task.all.order('created_at DESC')
   end
 
-  # GET /tasks/1
-  def show
-  end
+  def show; end
 
-  # GET /tasks/new
   def new
     @task = Task.new
   end
 
-  # GET /tasks/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /tasks
   def create
     @task = Task.new(task_params)
 
@@ -30,7 +23,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tasks/1
   def update
     if @task.update(task_params)
       redirect_to @task, notice: 'Task was successfully updated.'
@@ -39,20 +31,18 @@ class TasksController < ApplicationController
     end
   end
 
-  # DELETE /tasks/1
   def destroy
     @task.destroy
     redirect_to tasks_url, notice: 'Task was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def task_params
-      params.require(:task).permit(:title, :content, :limit, :status, :priority)
-    end
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :limit, :status, :priority, :content)
+  end
 end
---
+++ spec/system/task_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+RSpec.describe 'タスク管理機能', type: :system do
+  describe 'タスク一覧画面' do
+    before do
+      @task = FactoryBot.create(:task)
+    end
+    context 'タスクを作成した場合' do
+      it '作成済みのタスクが表示される' do
+        FactoryBot.create(:task)
+        visit tasks_path
+        expect(page).to have_content 'task'
+      end
+    end
+    context '複数のタスクを作成した場合' do
+      it 'タスクが作成日時の降順に並んでいる' do
+        new_task = FactoryBot.create(:second_task)
+        visit tasks_path
+        task_list = all('#task_row') # タスク一覧を配列として取得するため、View側でidを振っておく
+        expect(task_list[0]).to have_content 'new_task'
+        expect(task_list[1]).to have_content 'task'
+      end
+    end
+  end
+  describe 'タスク登録画面' do
+    context '必要項目を入力して、createボタンを押した場合' do
+      it 'データが保存される' do
+        visit new_task_path
+        fill_in 'Title', with: 'タスク'
+        fill_in 'Content', with: 'コンテンツ'
+        click_on '登録する'
+        expect(page).to have_content 'content', 'コンテンツ'
+      end
+    end
+  end
+  describe 'タスク詳細画面' do
+    context '任意のタスク詳細画面に遷移した場合' do
+      it '該当タスクの内容が表示されたページに遷移する' do
+        task = FactoryBot.create(:task)
+        visit tasks_path
+        click_on 'Show'
+        expect(page).to have_content 'title', 'task'
+      end
+    end
+  end
+end
