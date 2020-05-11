06b58968161d32977f4508bf36282a8dfec5e02c
ryohei udagawa <r.udagawa1995@gmail.com>
2020-05-11 17:25:38 +0900

    フィードバックメッセージ修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -17,7 +17,7 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to admin_users_path, notice: 'Task was successfully created.'
+      redirect_to admin_users_path, notice: 'User was successfully created.'
     else
       render :new
     end
@@ -27,15 +27,19 @@ class Admin::UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to admin_users_path, notice: 'Task was successfully updated.'
+      redirect_to admin_users_path, notice: 'User was successfully updated.'
     else
+      flash[:alert] = "編集に失敗しました。"
       render :edit
     end
   end
 
   def destroy
-    @user.destroy
-    redirect_to admin_users_path, notice: 'Task was successfully destroyed.'
+    if @user.destroy
+      redirect_to admin_users_path, notice: 'User was successfully destroyed.'
+    else
+      flash[:alert] = "削除に失敗しました。"
+    end
   end
 
   private
