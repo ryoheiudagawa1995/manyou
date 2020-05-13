d60fac9ce228bf3b028dde06d14902d07a33d626
ryohei udagawa <r.udagawa1995@gmail.com>
2020-05-13 10:29:24 +0900

    label検索修正

--- app/helpers/labels_helper.rb
+++ app/helpers/labels_helper.rb
@@ -1,2 +1,8 @@
 module LabelsHelper
+
+  def task_research(label_ids)
+    label_ids.each do |label|
+    Label.where(label_id: label).labelling_tasks
+  end
+
 end
--- app/models/label.rb
+++ app/models/label.rb
@@ -1,5 +1,5 @@
 class Label < ApplicationRecord
   has_many :labellings, dependent: :destroy
-  has_many :tasks, through: :labellings
+  has_many :labelling_tasks, through: :labellings, source: :task
 
 end
