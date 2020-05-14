1ee38d74fb40b212fbe2f8a985a5877e3eec4bd8
ryohei udagawa <r.udagawa1995@gmail.com>
2020-05-14 22:16:40 +0900

    update: フィードバックメッセージ（管理者画面）修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -17,7 +17,7 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to admin_users_path, notice: 'User was successfully created.'
+      redirect_to admin_users_path, notice: "#{@user.name}を追加しました。"
     else
       render :new
     end
@@ -27,7 +27,7 @@ class Admin::UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to admin_users_path, notice: 'User was successfully updated.'
+      redirect_to admin_users_path, notice: "#{@user.name}を編集しました。"
     else
       flash[:alert] = '編集に失敗しました。'
       render :edit
@@ -36,7 +36,7 @@ class Admin::UsersController < ApplicationController
 
   def destroy
     if @user.destroy
-      redirect_to admin_users_path, notice: 'User was successfully destroyed.'
+      redirect_to admin_users_path, notice: "#{@user.name}を削除しました。"
     else
       flash[:alert] = '削除出来ません。'
       redirect_to admin_users_path
