bfd542d64808f12c5880e2ff11a6eac0a453d8ef
ryohei udagawa <r.udagawa1995@gmail.com>
2020-05-15 16:54:35 +0900

    スクロール解除

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
+
 module Manyou
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
